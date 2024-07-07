8dfb5f33046cec6db0992f8c4de980137363b3ef
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 13:35:02 +0300

    Created nearby-search java project.

---
+++ src/test/java/com/codexist/nearbysearch/NearbySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexist.nearbysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
