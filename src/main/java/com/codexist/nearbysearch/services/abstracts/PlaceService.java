9283f78f390236383cdf19ac201d3c3597151f1b
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 22:21:05 +0300

    Redefined the method to send a request to the Google place api resource searchNearbyPlaces(double lat, double lng, int radius)

--- src/main/java/com/codexist/nearbysearch/services/abstracts/PlaceService.java
+++ src/main/java/com/codexist/nearbysearch/services/abstracts/PlaceService.java
@@ -1,10 +1,11 @@
 package com.codexist.nearbysearch.services.abstracts;
 
 import com.codexist.nearbysearch.dto.responses.NearbySearchResponse;
+import com.codexist.nearbysearch.dto.responses.PlaceResponseDto;
 
 import java.util.List;
 
 public interface PlaceService {
 
-    List<NearbySearchResponse> getLocations();
+    PlaceResponseDto searchNearbyPlaces(double lat, double lng, int radius) ;
 }
