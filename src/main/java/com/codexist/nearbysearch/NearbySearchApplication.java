3cf8adc846506be6546479f59e64f9b4b86c1343
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 15:43:45 +0300

    Initialize database with test Place entity on application startup.

--- src/main/java/com/codexist/nearbysearch/NearbySearchApplication.java
+++ src/main/java/com/codexist/nearbysearch/NearbySearchApplication.java
@@ -1,13 +1,39 @@
 package com.codexist.nearbysearch;
 
+import com.codexist.nearbysearch.models.Place;
+import com.codexist.nearbysearch.repositories.PlaceRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NearbySearchApplication {
+@AllArgsConstructor
+public class NearbySearchApplication implements CommandLineRunner {
+
+    private final PlaceRepository placeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(NearbySearchApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+       Place test =  Place.builder()
+                .placeId("ChIJAUn6iyYSKxURNGAXrEYxMBo")
+                .address("Gümüşsuyu, Sıraselviler Caddesi No:5")
+                .name("Taxim Hill Hotel")
+                .geometry(Place.Geometry.builder()
+                        .location(Place.Geometry.Location.builder()
+                                .lat(41.0364943)
+                                .lng(28.9854955)
+                                .build())
+                        .build())
+               .build();
+
+       this.placeRepository.save(test);
+
+
+
+    }
 }
