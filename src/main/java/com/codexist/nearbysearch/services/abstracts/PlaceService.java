07a2bc6ab0f2b89ebbf09a35e0054139920123f5
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 22:36:54 +0300

    API'den dönen sonuçların Place nesnesine dönüştürme  ve veri tabanına kaydedetme methodu tanımlandı.

--- src/main/java/com/codexist/nearbysearch/services/abstracts/PlaceService.java
+++ src/main/java/com/codexist/nearbysearch/services/abstracts/PlaceService.java
@@ -6,4 +6,6 @@ import com.codexist.nearbysearch.dto.responses.PlaceResponseDto;
 public interface PlaceService {
 
     PlaceResponseDto searchNearbyPlaces(double lat, double lng, int radius) ;
+
+    PlaceResponseDto savePlacesFromApiToDatabase(double latitude, double longitude, int radius);
 }
