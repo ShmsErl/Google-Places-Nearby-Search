ea77e4efa1f429f9546bc285fb4b9e3566f9b57a
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 15:25:27 +0300

    Added Place model with embedded Geometry and Location classes - Defined Place entity with JPA annotations - Embedded Geometry and Location classes using @Embeddable - Included fields for placeId, name, address, and geographic coordinates

---
+++ src/main/java/com/codexist/nearbysearch/models/Place.java
@@ -0,0 +1,54 @@
+package com.codexist.nearbysearch.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "places")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Place {
+    @Id
+    private String placeId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+
+    @Embedded
+    private Geometry geometry;
+
+
+    // Nested Geometry class
+    @Embeddable
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class Geometry {
+        @Embedded
+        private Location location;
+
+        // Nested Location class
+        @Embeddable
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Getter
+        @Setter
+        public static class Location {
+            private double lat;
+            private double lng;
+
+        }
+    }
+
+    public Place(String placeId, String name, String address, Geometry geometry) {
+        this.placeId = placeId;
+        this.name = name;
+        this.address = address;
+        this.geometry = geometry;
+    }
+
+
+}
