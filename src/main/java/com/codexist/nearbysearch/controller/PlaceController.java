decc667a1a45dec48af412b15fb6620ec4500799
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 22:55:15 +0300

    Added endpoint for retrieving nearby places.

--- src/main/java/com/codexist/nearbysearch/controller/PlaceController.java
+++ src/main/java/com/codexist/nearbysearch/controller/PlaceController.java
@@ -1,8 +1,12 @@
 package com.codexist.nearbysearch.controller;
 
+import com.codexist.nearbysearch.dto.responses.PlaceResponseDto;
 import com.codexist.nearbysearch.services.abstracts.PlaceService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +16,13 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
-
+    @GetMapping
+    public ResponseEntity<PlaceResponseDto> getNearbyPlaces(
+            @RequestParam double latitude,
+            @RequestParam double longitude,
+            @RequestParam int radius) {
+        PlaceResponseDto response = placeService.searchNearbyPlaces(latitude, longitude, radius);
+        return ResponseEntity.ok(response);
+    }
 
 }
