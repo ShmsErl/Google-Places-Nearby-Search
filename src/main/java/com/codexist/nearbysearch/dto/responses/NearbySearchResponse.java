da4b99583708838742620a99d1bebb7d51d55004
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 22:08:37 +0300

    Updated the name and content of the NearbySearch dto class according to the return type of the external api resource and mapped the fields.

--- src/main/java/com/codexist/nearbysearch/dto/responses/NearbySearchResponse.java
+++
@@ -1,9 +0,0 @@
-package com.codexist.nearbysearch.dto.responses;
-
-public record NearbySearchResponse(
-        String placeId,
-        String name,
-        String address,
-        double latitude,
-        double longitude) {
-}
---
+++ src/main/java/com/codexist/nearbysearch/dto/responses/PlaceDto.java
@@ -0,0 +1,57 @@
+package com.codexist.nearbysearch.dto.responses;
+
+import com.codexist.nearbysearch.models.Place;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+@Builder
+public class PlaceDto {
+
+    @JsonProperty("place_id")
+    private String placeId;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("vicinity")
+    private String vicinity;
+
+    @JsonProperty("geometry")
+    private Place.Geometry geometry;
+
+
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVicinity() {
+        return vicinity;
+    }
+
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    public Place.Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Place.Geometry geometry) {
+        this.geometry = geometry;
+    }
+}
