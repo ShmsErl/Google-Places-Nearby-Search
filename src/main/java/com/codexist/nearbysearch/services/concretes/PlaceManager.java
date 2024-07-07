2f15ae791aecd19df1a6000180d6fe38c59edf73
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 16:20:59 +0300

    Created PlaceManager class.

---
+++ src/main/java/com/codexist/nearbysearch/services/concretes/PlaceManager.java
@@ -0,0 +1,12 @@
+package com.codexist.nearbysearch.services.concretes;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PlaceManager {
+
+
+
+}
