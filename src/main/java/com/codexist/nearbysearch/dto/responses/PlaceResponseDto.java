8dcee25fe7c663b5119735351c4fbd5a6430d5a7
ShmsErl <erol.sehmus1@gmail.com>
2024-07-07 22:10:13 +0300

    In accordance with the response json structure of the Nearby-search apis, the dto class with the required fields has been defined.

---
+++ src/main/java/com/codexist/nearbysearch/dto/responses/PlaceResponseDto.java
@@ -0,0 +1,42 @@
+package com.codexist.nearbysearch.dto.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class PlaceResponseDto {
+
+    @JsonProperty("html_attributions")
+    private List<Object> htmlAttributions;
+
+    @JsonProperty("results")
+    private List<PlaceDto> results;
+
+    @JsonProperty("status")
+    private String status;
+
+
+    public List<Object> getHtmlAttributions() {
+        return htmlAttributions;
+    }
+
+    public void setHtmlAttributions(List<Object> htmlAttributions) {
+        this.htmlAttributions = htmlAttributions;
+    }
+
+    public List<PlaceDto> getResults() {
+        return results;
+    }
+
+    public void setResults(List<PlaceDto> results) {
+        this.results = results;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
