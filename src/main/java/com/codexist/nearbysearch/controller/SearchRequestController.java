ebce8e399bc1aefe5d0ea0185776088fb8ad7b33
ShmsErl <erol.sehmus1@gmail.com>
2024-07-09 21:43:15 +0300

    Added MySQL dependency and webconfig class.

--- src/main/java/com/codexist/nearbysearch/controller/SearchRequestController.java
+++ src/main/java/com/codexist/nearbysearch/controller/SearchRequestController.java
@@ -4,14 +4,12 @@ import com.codexist.nearbysearch.dto.responses.PlaceResponseDto;
 import com.codexist.nearbysearch.services.abstracts.SearchRequestService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/nearby-search")
 @AllArgsConstructor
+@CrossOrigin
 public class SearchRequestController {
 
     private final SearchRequestService searchRequestService;
